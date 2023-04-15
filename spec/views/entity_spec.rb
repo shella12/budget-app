0ca12f6e0e43e83d27a8de1e4ee86dc11b0de4e8
shella12 <ayeshaarshad4567@gmail.com>
2023-04-15 22:27:37 +0500

    Fix linter errors

--- spec/views/entity_spec.rb
+++ spec/views/entity_spec.rb
@@ -10,15 +10,14 @@ RSpec.describe 'expenses#new', type: :system do
       fill_in 'user[email]', with: 'ayesha@gmail.com'
       fill_in 'user[password]', with: 'topsecret'
       click_on 'Log in'
-      click_link("Add new category")
+      click_link('Add new category')
       sleep(1)
-      expect(current_path).to eql("/group/new")
+      expect(current_path).to eql('/group/new')
     end
 
     it 'new category form' do
       expect(page).to have_content('NEW CATEGORY')
       expect(page).to have_selector(:link_or_button, 'Add new category')
-    end  
-    
+    end
   end
 end
--- spec/views/group_spec.rb
+++ spec/views/group_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe 'expenses#new', type: :system do
   describe 'index page' do
     let(:group) { FactoryBot.create(:group) }
     before(:each) do
-        group.save
+      group.save
       @ayesha = User.create!(name: 'Ayesha', email: 'ayesha@gmail.com', password: 'topsecret',
                              password_confirmation: 'topsecret')
       @transaction = Entity.create(name: 'Over Time', amount: 200,
-                                                               author: @user)
-      @category_tansaction= EntityGroup.create(entity_id: @transaction.id, group: group)
-                           
+                                   author: @user)
+      @category_tansaction = EntityGroup.create(entity_id: @transaction.id, group:)
+
       visit '/'
       click_on 'Log in'
       fill_in 'user[email]', with: 'ayesha@gmail.com'
@@ -20,24 +20,23 @@ RSpec.describe 'expenses#new', type: :system do
     end
 
     it 'new category form' do
-        expect(group.icon).to be_an_instance_of(ActiveStorage::Attached::One)
-        expect(page).to have_content(group.name)
-        expect(page).to have_content("$0")
-        expect(page).to have_content(group.created_at.strftime("%d %b %Y"))
-        click_on "#{group.name}"
-        sleep(1)
-        expect(current_path).to eql("/group/#{group.id}")
-        expect(page).to have_content('DETAIL')
-        expect(page).to have_selector(:link_or_button, 'Add new transaction')
-        click_on "Add new transaction"
-        sleep(1)
-        expect(current_path).to eql("/entity/new")
-        expect(page).to have_selector(:link_or_button, 'Add Transaction')
-        fill_in 'name', with: 'Mcdonald'
-        fill_in 'amount', with: 8.9
-        page.check("#{group.name}")
-        click_on "Add Transaction"
-    end  
-    
+      expect(group.icon).to be_an_instance_of(ActiveStorage::Attached::One)
+      expect(page).to have_content(group.name)
+      expect(page).to have_content('$0')
+      expect(page).to have_content(group.created_at.strftime('%d %b %Y'))
+      click_on group.name.to_s
+      sleep(1)
+      expect(current_path).to eql("/group/#{group.id}")
+      expect(page).to have_content('DETAIL')
+      expect(page).to have_selector(:link_or_button, 'Add new transaction')
+      click_on 'Add new transaction'
+      sleep(1)
+      expect(current_path).to eql('/entity/new')
+      expect(page).to have_selector(:link_or_button, 'Add Transaction')
+      fill_in 'name', with: 'Mcdonald'
+      fill_in 'amount', with: 8.9
+      page.check(group.name.to_s)
+      click_on 'Add Transaction'
+    end
   end
 end
